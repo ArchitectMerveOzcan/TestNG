302941276fd067084f5f05892468b05feb35eab9
Merve Özcan <ozcanmerve.tr@gmail.com>
2023-01-12 13:50:09 +0300

    day18

---
+++ src/test/java/techproed/pages/HomePage.java
@@ -0,0 +1,18 @@
+package techproed.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
+
+public class HomePage {
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(partialLinkText = "Login")
+    public WebElement homePageLoginLink;
+
+    @FindBy(id = "dropdown-basic-button")
+    public WebElement userID;
+}
