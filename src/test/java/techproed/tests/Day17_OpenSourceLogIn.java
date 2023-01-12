b5f4506b52949ebcc0dc6a86bee4d9a87c2d40b4
Merve Özcan <ozcanmerve.tr@gmail.com>
2023-01-12 12:55:12 +0300

    merve

---
+++ src/test/java/techproed/tests/Day17_OpenSourceLogIn.java
@@ -0,0 +1,31 @@
+package techproed.tests;
+
+import org.testng.annotations.Test;
+import techproed.pages.OpenSourcePage;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
+
+public class Day17_OpenSourceLogIn {
+    /*
+    POM:
+    1.Locate the page object in the pages package
+    -username
+    -password
+    -button
+    Note: Use constructor to instantiate page objects
+
+    2.In test class, create page object to access the objects in the page class
+     */
+    @Test
+    public  void loginTest(){
+        OpenSourcePage openSourcePage = new OpenSourcePage();
+        Driver.getDriver().get(ConfigReader.getProperty("open_source_URL"));
+
+        openSourcePage.userName.sendKeys("Admin");
+        openSourcePage.password.sendKeys("admin123");
+        openSourcePage.submitButton.sendKeys("admin123");
+    }
+//    Create a test case to login the https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+//    using page object model
+
+}
